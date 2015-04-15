73a3e452555e5cd17f7fd1106997282f44e2f2d9
zenzontle <zenzontle@gmail.com>
2015-04-15 16:59:51 -0700

    Add Comment page

---
+++ Unkcon/Controllers/CommentController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Unkcon.Controllers
+{
+    public class CommentController : Controller
+    {
+        //
+        // GET: /Comment/
+        public ActionResult Comment()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<ActionResult> Publish(string returnUrl)
+        {
+            //TODO Plugin to EF
+
+            return RedirectToLocal(returnUrl);
+        }
+
+        private ActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+	}
+}
---
+++ Unkcon/Models/CommentModels.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Unkcon.Models
+{
+    public class CommentModels
+    {
+        [Required]
+        [Display(Name = "Comment")]
+        public string Comment { get; set; }
+
+    }
+}
