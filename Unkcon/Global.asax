87033a528852d5f3684baafd44d6af9d98f8046b
zenzontle <zenzontle@gmail.com>
2015-04-15 15:08:42 -0700

    Add initial Web App structure

---
+++ Unkcon/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Unkcon.MvcApplication" Language="C#" %>
---
+++ Unkcon/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Unkcon.Startup))]
+namespace Unkcon
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
