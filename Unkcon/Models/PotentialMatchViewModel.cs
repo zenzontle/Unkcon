11116fb11f2aca972e66f8811450d989312c4b93
zenzontle <zenzontle@gmail.com>
2015-05-05 14:48:29 -0700

    Add PotentialMatchViewModel Add way to see other people's comments Changed Comment view to return own coments Clean up some stuff from the initial project template

---
+++ Unkcon/Models/PotentialMatchViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Unkcon.Models
+{
+    public class PotentialMatchViewModel
+    {
+        [Required]
+        public string Comment { get; set; }
+
+        [Required]
+        public string Reply { get; set; }
+    }
+}
