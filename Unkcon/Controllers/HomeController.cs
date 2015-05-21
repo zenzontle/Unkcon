b41ead96ebc4595470efac5f7c6c9893bb7d5ec8
zenzontle <zenzontle@gmail.com>
2015-05-20 20:39:58 -0700

    Clean up original template. Remove Contact page.

--- Unkcon/Controllers/HomeController.cs
+++ Unkcon/Controllers/HomeController.cs
@@ -15,14 +15,7 @@ namespace Unkcon.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Tinder for messages/thoughts.";
 
             return View();
         }
